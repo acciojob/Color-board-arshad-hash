9be668e8007a68595f64bc1d54c6af9f42463d60
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-12-27 23:49:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+  document.addEventListener("DOMContentLoaded" , function (){
+            let container = document.querySelector(".container");
+            
+            for(let i=0  ; i<800 ; i++)
+            {
+                let pixel= document.createElement("div");
+                pixel.classList.add("box");
+                container.appendChild(pixel);
+
+                pixel.addEventListener("mouseover" , ()=>{
+                pixel.style.background="red";
+            });
+            pixel.addEventListener("mouseout" , ()=>{
+
+                setTimeout(()=>{
+                    pixel.style.background="grey";
+                },1000);
+                
+            });
+            }
+           
+        });
+        
